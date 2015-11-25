93d80fe6e788c3c09ca313a20b9c5707e89be935
vpoulain <vincent.poulain2@gmail.com>
2015-11-26 00:34:04 +0100

    Added filter view about category

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView
 
-from .models import Project
+from .models import Project, Category
 
 
 class ProjectIndexView(ListView):
@@ -8,10 +8,12 @@ class ProjectIndexView(ListView):
 
     def get_queryset(self):
         query = self.request.GET.get('search')
+        category = self.request.GET.get('category')
 
         if query:
-
             return Project.search(query)
+        elif category:
+            return Category.objects.get(name=category).projects.all()
 
         return Project.objects.all()
 
