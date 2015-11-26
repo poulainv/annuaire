6c6b4fcce0f8b1578e7bbab9449694b7f51677b8
vpoulain <vincent.poulain2@gmail.com>
2015-11-26 13:19:08 +0100

    Added CSS and sub categories filtering

--- app/views.py
+++ app/views.py
@@ -9,21 +9,27 @@ class ProjectIndexView(ListView):
     def get_queryset(self):
         query = self.request.GET.get('search')
         category = self.request.GET.get('category')
+        sub_cat = self.request.GET.get('sub_cat')
 
         if query:
             return Project.search(query)
-        elif category:
-            return Category.objects.get(name=category).projects.all()
+        elif category and not sub_cat:
+            return Category.objects.get(name=category)\
+                .projects.all()
+        elif category and sub_cat:
+            return Category.objects.get(name=category)\
+                .projects.filter(sub_categories__pk=sub_cat)
 
         return Project.objects.all()
 
     def get_context_data(self, **kwargs):
         context = super(ProjectIndexView, self).get_context_data(**kwargs)
-        projects = self.get_queryset()
-        sub_cats = []
-        for p in projects:
-            sub_cats.extend(p.sub_categories.all())
-        context['sub_categories'] = set(sub_cats)
+        category_name = self.request.GET.get('category')
+        if category_name:
+            category = Category.objects.get(name=category_name)
+            context['category'] = category
+            context['sub_categories'] = category\
+                .sub_categories.all()
         return context
 
 
