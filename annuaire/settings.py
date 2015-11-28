f4fae64777cff8a26a1ecc08af80b106df0e6d8f
vpoulain <vincent.poulain2@gmail.com>
2015-11-28 21:59:03 +0100

    Disable debug mode for production

--- annuaire/settings.py
+++ annuaire/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yi0cvdox+5vre4ny&k8up_*7sj$ylnqe-uqcu$b85w&%dk%9e6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
