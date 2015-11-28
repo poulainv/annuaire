30ec0eed81894596e9995210878197c22c7ea69f
vpoulain <vincent.poulain2@gmail.com>
2015-11-28 17:43:37 +0100

    Added admin route + fix orange colour

--- app/models.py
+++ app/models.py
@@ -58,6 +58,7 @@ class Project(models.Model):
     contact_name = models.CharField(max_length=250, blank=True)
     contact_telephone = models.CharField(max_length=20, blank=True)
     contact_mail = models.EmailField(blank=True)
+    blog_article_url = models.URLField(blank=True)
 
     def __str__(self):
         return self.title.capitalize()
