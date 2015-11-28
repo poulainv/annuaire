71fae358c85be04d58502d9020ba59d82d08ef33
vpoulain <vincent.poulain2@gmail.com>
2015-11-29 00:56:27 +0100

    Add link to sumission page

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     url(r'^s3direct/', include('s3direct.urls')),
-    url(r'^submission/', views.submit_project),
+    url(r'^submission/', views.submit_project, name='submission'),
     url(r'^projects/(?P<pk>[0-9]+)$', views.ProjectDetailView.as_view(), name='project'),
     url(r'^projects', views.ProjectIndexView.as_view(), name='projects'),
     url(r'', TemplateView.as_view(template_name="app/index.html"), name='home')
