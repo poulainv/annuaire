ed9b07d987631cd7dd94b03cf6e7b01bd8f8c1a0
vpoulain <vincent.poulain2@gmail.com>
2016-02-26 13:03:34 +0100

    Removed directory static file

--- annuaire/settings.py
+++ annuaire/settings.py
@@ -149,8 +149,8 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'app', 'static')
+    # os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'app', 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
