02f8173017e8fafc5c7bc5969327006799016b05
vpoulain <vincent.poulain2@gmail.com>
2016-05-19 19:47:33 +0200

    Fix facebook connexion

--- app/pipelines.py
+++ app/pipelines.py
@@ -5,4 +5,4 @@ def save_profile_picture(backend, user, response, details, is_new=False, *args, 
     
     if backend.__class__.__name__ == 'FacebookOAuth2' and is_new:
         url = 'http://graph.facebook.com/{0}/picture'.format(response['id'])
-        UserProfile.objects.create(user=user, picture_url=url)
+        UserProfile.objects.update_or_create(user=user, picture_url=url)
